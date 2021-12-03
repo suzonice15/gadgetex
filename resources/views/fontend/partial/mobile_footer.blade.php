ce83edfe38c317f624c520c36d2391cb9d7f6ccc
Shahinul islam  sujon <suzonice15@gmail.com>
2021-12-03 20:24:17 +0600

    header footer both completed

--- resources/views/fontend/partial/mobile_footer.blade.php
+++ resources/views/fontend/partial/mobile_footer.blade.php
@@ -1,5 +1,5 @@
 
-<div class="container-fluid footer-mobile p-5" style="background-color: #ddd">
+<div class="container-fluid footer-mobile pt-1" style="background-color: #ddd">
 
     <div class="row">
         <div class="col-4 ">
@@ -60,36 +60,71 @@
 
 </div>
 
-<div class="container-fluid footer-mobile p-5" >
+<div class="container-fluid footer-mobile" >
 
 <div class="row">
     <div class="col-12 text-center">
         <img style="height: 61px;" src="{{asset('/images/ICON/logo.svg')}}" class="img-fluid">
         <p>GadgetEx continues to provide customers with a reliable and trustworthy online shop along with their two shops, helping to ensure a better and premium business experience.</p>
         <hr>
-        <div class="d-flex flex-row justify-content-center">
-              <img src="{{asset('/images/ICON/facebook.png')}}"   class="img-fluid  "/>
-            <img src="{{asset('/images/ICON/youtube.png')}}"   class="img-fluid  "/>
-            <img src="{{asset('/images/ICON/whatsapp.png')}}"   class="img-fluid "/>
-            <img src="{{asset('/images/ICON/imo.png')}}"  class="img-fluid  "/>
+        <ul class="socialli">
+        <div class="d-flex flex-row justify-content-center ">
+                       <a class="solik" href="http://"> <i class="fab fa-facebook-square  social-padding-of-single-product"></i></a>
+                        <a class="solik" style="color: #833AB4;" href="http://" target="_blank" rel="noopener noreferrer"> <i class="fab fa-instagram  social-padding-of-single-product"></i></a>
+                        <a class="solik" style="color:#25D366;" href="http://" target="_blank" rel="noopener noreferrer"><i class="fab fa-whatsapp  social-padding-of-single-product"></i></a>
+                        <a class="solik" style="color:#2867B2;" href="http://" target="_blank" rel="noopener noreferrer"> <i class="fab fa-linkedin  social-padding-of-single-product "></i></a>
+                       <a class="solik" style="color:#1DA1F2;" href="http://" target="_blank" rel="noopener noreferrer"> <i class="fab fa-twitter  social-padding-of-single-product"></i></a>
+         </div>
+        </ul>
 
-        </div>
-
-        <h4 class="mt-5">@ {{date("Y")}} .All  Rights Reserved by GadgetEx Developed by <a href="http://it.jibonpata.com" style="text-decoration: none;color:black" target="_blank">Jibonpata IT Ltd</a></h4>
+      
 
 
     </div>
 </div>
+<div class="row mt-5">
+    <div class="col-12 mt-5">
+        <h4 class="fbytitle">Follow Facebook Page</h4>
+        <div class="fbycover">
+            <!-- <img class="img-fluid" src="{{asset('/images/ICON/fbcover.png')}}"> -->
+
+        </div>
+    </div>
+    <div class="col-12b mt-5">
+    <h4 class="fbytitle">Subscribe YouTube Channel</h4>
+    <div class="fbycover">
+    <div class="embed-responsive embed-responsive-16by9">
+    <iframe width="100%" height="146" src="https://www.youtube.com/embed/BVMsRltq2yU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+        </div>
+    </div>
+</div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+        <h4 style="font-size: 12px;" class="mt-5 text-center">@ {{date("Y")}} .All  Rights Reserved by GadgetEx Developed by <a href="http://it.jibonpata.com" style="text-decoration: none;color:black" target="_blank">Jibonpata IT Ltd</a></h4>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+        <h2 class="ecab">Registered Member of <img src="{{asset('/images/ICON/Member of eCab.png')}}" style="height: 32px;margin-top: 2px;margin-left: 9px;" class="img-fluid"> </h2>
+        </div>
+        <div class="col-12">
+        <img src="{{asset('/images/ICON/Payment Partner-01 1.png')}}"   class="img-fluid">
+        </div>
+    </div>
 </div>
 
-<div class="container-fluid">
+
+<div class="container-fluid mt-3">
     <div class="row">
         <div class="col-2" style="background:#ddd;border-top-left-radius: 30px;">
             <img src="{{asset('/images/ICON/Capture 21.png')}}" class="img-fluid "  >
 
         </div>
         <div class="col-8 text-center" style="background-color: #3C3C3C">
-           <p style="cursor:pointer;color:white;padding-top: 28px;font-size: 34px;font-weight: bold;">Start Purchase</p>
+           <p class="fbotom" >Start Purchase</p>
         </div>
         <div class="col-2" style="background:#ddd;border-top-right-radius: 30px;">
             <img src="{{asset('/images/ICON/Messenger 8.png')}}" class="img-fluid "  style="margin-top: 13px;" >
