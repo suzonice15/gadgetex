1499970964ca847f50cdbbdf2b321bf9ac8946de
Shahinul islam  sujon <suzonice15@gmail.com>
2021-12-04 16:32:25 +0600

    home page 3 section design

--- resources/views/fontend/myoffer/myoffer.blade.php
+++ resources/views/fontend/myoffer/myoffer.blade.php
@@ -55,5 +55,11 @@
         </div>
     </div>
 </section>
-.
+<section class="campaining">
+    <div class="container">
+        <div class="row">
+            .
+        </div>
+    </div>
+</section>
 @endsection
