b545ebe807c437cec69a26179e41325682ee4a53
Shahinul islam  sujon <suzonice15@gmail.com>
2021-12-16 16:44:11 +0600

    searcresult

--- resources/views/fontend/partial/desktop_header_logo.blade.php
+++ resources/views/fontend/partial/desktop_header_logo.blade.php
@@ -16,6 +16,71 @@
                 </div>
             </div>
             <ul class="desktop-search-menu">
+            <div class="searchresult">
+                    <ul>
+                        <li>
+                            <div class="docname">
+                            <a href="https://fairmart.com.bd/item/parachute-skinpure-beauty-olive-oil-100ml-bzt3873in4">
+                                <div class="search-content">
+                                <img src="https://fairmartslive.s3.ap-south-1.amazonaws.com/public/assets/images/thumbnails/uwtVgrtF4QD9.jpg" alt="">
+                                    <p><span>Parachute SkinPure Be<b>aut</b>y Olive Oil 100ml</span> <br>
+                                    <span style="font-size: 14px; font-weight:600; display:block;"> 
+                                        <del>৳200</del> ৳190
+                                     </span>
+                                </p>
+                                </div>
+                                
+                            </a>
+	                        </div>
+                        </li>
+                        <li>
+                            <div class="docname">
+                            <a href="https://fairmart.com.bd/item/parachute-skinpure-beauty-olive-oil-100ml-bzt3873in4">
+                                <div class="search-content">
+                                <img src="https://fairmartslive.s3.ap-south-1.amazonaws.com/public/assets/images/thumbnails/uwtVgrtF4QD9.jpg" alt="">
+                                    <p><span>Parachute SkinPure Be<b>aut</b>y Olive Oil 100ml</span> <br>
+                                    <span style="font-size: 14px; font-weight:600; display:block;"> 
+                                        <del>৳200</del> ৳190
+                                     </span>
+                                </p>
+                                </div>
+                                
+                            </a>
+	                        </div>
+                        </li>
+                        <li>
+                            <div class="docname">
+                            <a href="https://fairmart.com.bd/item/parachute-skinpure-beauty-olive-oil-100ml-bzt3873in4">
+                                <div class="search-content">
+                                <img src="https://fairmartslive.s3.ap-south-1.amazonaws.com/public/assets/images/thumbnails/uwtVgrtF4QD9.jpg" alt="">
+                                    <p><span>Parachute SkinPure Be<b>aut</b>y Olive Oil 100ml</span> <br>
+                                    <span style="font-size: 14px; font-weight:600; display:block;"> 
+                                        <del>৳200</del> ৳190
+                                     </span>
+                                </p>
+                                </div>
+                                
+                            </a>
+	                        </div>
+                        </li>
+                        <li>
+                            <div class="docname">
+                            <a href="https://fairmart.com.bd/item/parachute-skinpure-beauty-olive-oil-100ml-bzt3873in4">
+                                <div class="search-content">
+                                <img src="https://fairmartslive.s3.ap-south-1.amazonaws.com/public/assets/images/thumbnails/uwtVgrtF4QD9.jpg" alt="">
+                                    <p><span>Parachute SkinPure Be<b>aut</b>y Olive Oil 100ml</span> <br>
+                                    <span style="font-size: 14px; font-weight:600; display:block;"> 
+                                        <del>৳200</del> ৳190
+                                     </span>
+                                </p>
+                                </div>
+                                
+                            </a>
+	                        </div>
+                        </li>
+                    </ul>
+
+                </div>
             </ul>
         </form>
     </div>
